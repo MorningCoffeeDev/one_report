89f6849c31eb4d3c7a4e1d97bfb54005b327fbe0
qinmingyuan <mingyuan0715@foxmail.com>
2015-08-10 15:43:42 +0800

    feat: if host application has no layout, use self

--- lib/one_report/engine.rb
+++ lib/one_report/engine.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'prawn/table'
 require 'refile'
 require 'refile/rails'
+require 'sprockets/railtie'
 
 module OneReport
   class Engine < ::Rails::Engine
-
+    config.assets.precompile += ['one_report/application.js']
+    config.assets.precompile += ['one_report/application.css']
   end
 end
 
