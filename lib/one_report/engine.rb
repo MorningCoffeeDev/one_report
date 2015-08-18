dd0ccd1df011bd5d1037ec73647d5582a0abf957
qinmingyuan <mingyuan0715@foxmail.com>
2015-08-18 09:26:07 +0800

    engine config assets did not support 3.2

--- lib/one_report/engine.rb
+++ lib/one_report/engine.rb
@@ -6,8 +6,8 @@ require 'sprockets/railtie'
 
 module OneReport
   class Engine < ::Rails::Engine
-    config.assets.precompile += ['one_report/application.js']
-    config.assets.precompile += ['one_report/application.css']
+    #config.assets.precompile += ['one_report/application.js']
+    #config.assets.precompile += ['one_report/application.css']
   end
 end
 
